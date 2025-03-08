21526cae25da23c67fd9707d35028979578e9ec7
Nicholas Seney <nseney1@gmail.com>
2025-03-07 23:40:04 -0500

    some clock stuff, idk

--- Environment/clock.py
+++ Environment/clock.py
@@ -1,9 +1,12 @@
 
+from typing import Dict, List
+from threading import Event, Thread
 import pygame
 from pygame.time import Clock
 
 
 class GameClock():
+    threads: Dict = {}
     gameClock: Clock = pygame.time.Clock()
     
     def __init__(self) -> None:
@@ -11,6 +14,19 @@ class GameClock():
 
     def tickClock(self, tick: int) -> None:
         self.gameClock.tick(tick)
+
+    def createSubProcess(self, name: str, event, handler) -> Thread:
+        thread = Thread(target=handler, args=(event,), name=name)
+        self.threads[name] = { "event": event, "handler": handler, "thread": thread }
+        return thread
+
+    def startSubProcess(self, name: str) -> Thread:
+        self.threads[name].thread.start()
+        return self.threads[name].thread.start()
+
+    def endSubProcess(self, name: str) -> None:
+        self.threads[name].thread.join()
+        del self.threads[name]
 
     pass
 
